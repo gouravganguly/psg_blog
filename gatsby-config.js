ea387cd93137cfd2c1fc3131fbcaa759eaf0fd31
gouravganguly <gourav1ganguly@gmail.com>
2020-05-10 20:19:35 +0100

    Update app structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Data Analysis Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Parthasarathy Ganguly`,
+      summary: `who lives teach and work on Data Analysis`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://parthasarathyganguly.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `parthasarathyganguly`,
     },
   },
   plugins: [
